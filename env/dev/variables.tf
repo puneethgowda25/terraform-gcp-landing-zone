3f158d6071f88e62505e39146fd25792bfc2a8b7
Puneeth Gowda <puneethgowda2525@gmail.com>
2026-02-23 20:23:46 +0530

    solve state lock issue

--- env/dev/variables.tf
+++ env/dev/variables.tf
@@ -21,69 +21,69 @@ variable "subnet_configs" {
   }))
 }
 
-# VM variables
-variable "vm_name_prefix" {
-  type = string
-}
+# # VM variables
+# variable "vm_name_prefix" {
+#   type = string
+# }
 
-variable "vm_machine_type" {
-  type = string
-}
+# variable "vm_machine_type" {
+#   type = string
+# }
 
-variable "vm_image" {
-  type = string
-}
+# variable "vm_image" {
+#   type = string
+# }
 
-variable "vm_target_size" {
-  type = number
-}
+# variable "vm_target_size" {
+#   type = number
+# }
 
-# Firewall variables: public HTTP
-variable "firewall_name" {
-  type = string
-}
+# # Firewall variables: public HTTP
+# variable "firewall_name" {
+#   type = string
+# }
 
-variable "firewall_ports" {
-  type = list(string)
-}
+# variable "firewall_ports" {
+#   type = list(string)
+# }
 
-variable "firewall_source_ranges" {
-  type = list(string)
-}
+# variable "firewall_source_ranges" {
+#   type = list(string)
+# }
 
-variable "firewall_target_tags" {
-  type = list(string)
-}
+# variable "firewall_target_tags" {
+#   type = list(string)
+# }
 
-# Firewall variables: GCP Health Checks
-variable "firewall_hc_name" {
-  type = string
-}
+# # Firewall variables: GCP Health Checks
+# variable "firewall_hc_name" {
+#   type = string
+# }
 
-variable "firewall_hc_ports" {
-  type = list(string)
-}
+# variable "firewall_hc_ports" {
+#   type = list(string)
+# }
 
-variable "firewall_hc_source_ranges" {
-  type = list(string)
-}
+# variable "firewall_hc_source_ranges" {
+#   type = list(string)
+# }
 
-variable "firewall_hc_target_tags" {
-  type = list(string)
-}
+# variable "firewall_hc_target_tags" {
+#   type = list(string)
+# }
 
-# Load balancer variables
-variable "lb_name" {
-  type = string
-}
+# # Load balancer variables
+# variable "lb_name" {
+#   type = string
+# }
 
-variable "lb_port" {
-  type    = number
-  default = 80
-}
+# variable "lb_port" {
+#   type    = number
+#   default = 80
+# }
 
-# Link to MIG instance group
-variable "lb_instance_group" {
-  type        = string
-  description = "Self-link of the Managed Instance Group (MIG) to attach the LB backend"
-}
+# # Link to MIG instance group
+# variable "lb_instance_group" {
+#   type        = string
+#   description = "Self-link of the Managed Instance Group (MIG) to attach the LB backend"
+# }
